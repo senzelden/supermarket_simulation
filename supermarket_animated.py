603485638165613c9857f42213c6b04bcbd0a762
senzelden <sunless@gmx.net>
2020-07-09 15:48:42 +0200

    Update for pathing implementation

--- supermarket_animated.py
+++ supermarket_animated.py
@@ -44,9 +44,9 @@ def visit_all_aisles(start_x, start_y):
         13: ['right', 650],
         14: ['down', 80]
     }
-    for key, value in commands.items():
-        for _ in range(value[1]):
-            x, y = pathing(value[0], x, y)
+    for command in commands.values():
+        for _ in range(command[1]):
+            x, y = pathing(command[0], x, y)
             yield x, y
 
 
@@ -74,9 +74,9 @@ def visit_dairy():
         7: ['right', 110],
         8: ['down', 110]
     }
-    for key, value in commands.items():
-        for _ in range(value[1]):
-            x, y = pathing(value[0], x, y)
+    for command in commands.values():
+        for _ in range(command[1]):
+            x, y = pathing(command[0], x, y)
             yield x, y
 
 
