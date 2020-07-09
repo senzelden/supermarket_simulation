9a3b8f8863a6cb29f30d332d21337b7b6af28b1c
senzelden <sunless@gmx.net>
2020-07-09 12:28:03 +0200

    Add np.random.poisson for distribution at locations

--- supermarket_animated.py
+++ supermarket_animated.py
@@ -173,7 +173,7 @@ while True:
     locations = [checkout, dairy, drinks, fruit, spices]
     updated_customers_present = cdf[cdf['time'] == current_time]['customers_present'].values
     for i, location in enumerate(locations):
-        location.customers_present = round(updated_customers_present[i],0).astype(int)
+        location.customers_present = np.random.poisson(updated_customers_present[i])
         for j in range(location.customers_present):
             new_x = pos[location.name][j]['x']
             new_y = pos[location.name][j]['y']
@@ -200,67 +200,3 @@ cv2.destroyAllWindows()
 # distance between aisles = 230
 # distance between position horizontal = 90
 # distance between position vertical = 60
-
-# checkout
-pos_C1 = [120,600]
-pos_C2 = [190,600]
-pos_C3 = [350,600]
-pos_C4 = [420,600]
-pos_C5 = [580,600]
-pos_C6 = [650,600]
-pos_C7 = [120,555]
-pos_C8 = [190,555]
-pos_C9 = [350,555]
-pos_C10 = [420,555]
-pos_C11 = [580,555]
-pos_C12 = [650,555]
-
-# entrance
-pos_E1 = [800,700]
-pos_E2 = [845,700]
-pos_E3 = [890,700]
-pos_E4 = [935,700]
-pos_E5 = [800,655]
-pos_E6 = [845,655]
-pos_E7 = [890,655]
-pos_E8 = [935,655]
-
-#fruit
-pos_F1 = [800,200]
-pos_F2 = [890,200]
-pos_F3 = [800,260]
-pos_F4 = [890,260]
-pos_F5 = [800,320]
-pos_F6 = [890,320]
-pos_F7 = [800,380]
-pos_F8 = [890,380]
-
-#spices
-pos_S1 = [570,200]
-pos_S2 = [660,200]
-pos_S3 = [570,260]
-pos_S4 = [660,260]
-pos_S5 = [570,320]
-pos_S6 = [660,320]
-pos_S7 = [570,380]
-pos_S8 = [660,380]
-
-#dairy
-pos_DA1 = [340,200]
-pos_DA2 = [430,200]
-pos_DA3 = [340,260]
-pos_DA4 = [430,260]
-pos_DA5 = [340,320]
-pos_DA6 = [430,320]
-pos_DA7 = [340,380]
-pos_DA8 = [430,380]
-
-#drinks
-pos_DR1 = [110,200]
-pos_DR2 = [200,200]
-pos_DR3 = [110,260]
-pos_DR4 = [200,260]
-pos_DR5 = [110,320]
-pos_DR6 = [200,320]
-pos_DR7 = [110,380]
-pos_DR8 = [200,380]
