118563bd43a33e90514c56290ddf61f50b90b56e
senzelden <sunless@gmx.net>
2020-07-10 12:04:30 +0200

    structure program according to guideline

--- supermarket_animated.py
+++ supermarket_animated.py
@@ -3,7 +3,27 @@ import time
 import numpy as np
 import cv2
 import pandas as pd
-from positions import pos
+
+
+CUSTOMER_IMAGE_PATH = "final_logo.png"
+BACKGROUND_IMAGE_PATH = "resized_market.png"
+SUPERMARKET_LOGO_PATH = "resized_doodl.png"
+PRESENCE_PROBABILITIES_PATH = "customers_in_section.csv"
+ENTRANCE_PROBABILITIES_PATH = "avg_at_entrance.csv"
+POSITIONS = pd.read_json("positions.json")
+START_TIME = "08:00:00"
+
+
+def write_text(text, x_position, y_position):
+    cv2.putText(
+        frame,
+        str(text),
+        (x_position, y_position),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 0),
+        2,
+    )
 
 
 class Customer:
@@ -19,9 +39,9 @@ class Customer:
     def __repr__(self):
         return f"<Customer at {self.x}/{self.y}>"
 
-    def draw(self, frame):
-        frame[
-            self.y : self.y + self.image.shape[0], self.x : self.x + self.image.shape[1]
+    def draw(self, background_image):
+        background_image[
+            self.y: self.y + self.image.shape[0], self.x: self.x + self.image.shape[1]
         ] = self.image
 
 
@@ -39,6 +59,10 @@ class Location:
         return f"<{self.customers_present} customers present at section {self.name}.>"
 
 
+img = cv2.imread(CUSTOMER_IMAGE_PATH)
+background = cv2.imread(BACKGROUND_IMAGE_PATH)
+doodl = cv2.imread(SUPERMARKET_LOGO_PATH)
+
 checkout = Location("checkout", 0, 0)
 dairy = Location("dairy", 0, 5)
 drinks = Location("drinks", 0, 6)
@@ -46,32 +70,25 @@ fruit = Location("fruit", 0, 4)
 spices = Location("spices", 0, 3)
 entrance = Location("entrance", 0, 0)
 
-img = cv2.imread("final_logo.png")
-background = cv2.imread("resized_market.png")
-doodl = cv2.imread("resized_doodl.png")
 customers = []
-
-possible_times = pd.date_range(
-    pd.to_datetime("2020-07-10 07:00"), pd.to_datetime("2020-07-10 21:50"), freq="Min",
-)
-current_time = pd.to_datetime("08:00:00")
-expected_time_1 = pd.to_datetime("2020-07-10 07:00").time()
-expected_time_2 = pd.to_datetime("2020-07-10 07:01").time()
-
-cdf = pd.read_csv("customers_in_section.csv")
-entrance_df = pd.read_csv("avg_at_entrance.csv")
-cdf["time"] = pd.to_datetime(cdf["time"])
-entrance_df["time"] = pd.to_datetime(entrance_df["time"])
+locations = [checkout, dairy, drinks, fruit, spices]
 total_revenue = 0
+
+current_time = pd.to_datetime(START_TIME)
+
+df_presences = pd.read_csv(PRESENCE_PROBABILITIES_PATH)
+df_entrance = pd.read_csv(ENTRANCE_PROBABILITIES_PATH)
+df_presences["time"] = pd.to_datetime(df_presences["time"]).copy()
+df_entrance["time"] = pd.to_datetime(df_entrance["time"]).copy()
+
 
 while True:
     time.sleep(1)
     frame = background.copy()
-    locations = [checkout, dairy, drinks, fruit, spices]
-    updated_customers_present = cdf[cdf["time"] == current_time][
+    updated_customers_present = df_presences[df_presences["time"] == current_time][
         "customers_present"
     ].values
-    updated_customers_at_entrance = entrance_df[entrance_df["time"] == current_time][
+    updated_customers_at_entrance = df_entrance[df_entrance["time"] == current_time][
         "at_entrance"
     ].values
 
@@ -82,64 +99,50 @@ while True:
             )
         else:
             location.customers_present = np.random.poisson(updated_customers_present[i])
-        # remove values that are larger than maximum number of customers in aisle that can be visualized
+        # precent IndexError for POSITIONS dict
         if location.customers_present > 8:
             location.customers_present = 8
         for j in range(int(location.customers_present)):
-            new_x = pos[location.name][j]["x"]
-            new_y = pos[location.name][j]["y"] + 136
+            new_x = POSITIONS[location.name][int(j)]["x"]
+            new_y = POSITIONS[location.name][int(j)]["y"] + 136
             if j % 2 == 0:
                 new_img = img.copy()
             else:
-                new_img = img[:,::-1,:].copy()
+                new_img = img[:, ::-1, :].copy()
             customers.append(Customer(new_img, new_x, new_y))
         total_revenue += int(location.customers_present * location.revenue_per_minute)
     for customer in customers:
         customer.draw(frame)
 
-    # Add clock to frame
-    cv2.putText(
-        frame,
-        str(current_time.time()),
-        (10, 30),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 0, 0),
-        2,
+    write_text(current_time.time(), 10, 30)
+    write_text(f"Total Revenue: {total_revenue}EUR", 10, 30 + 100)
+    customers_screen_text = (
+        f"""
+        Customers:
+        Checkout: {checkout.customers_present}
+        Dairy: {dairy.customers_present}
+        Drinks: {drinks.customers_present}
+        Fruit: {fruit.customers_present}
+        Spices: {spices.customers_present}
+        """
     )
-    cv2.putText(
-        frame,
-        f"Total Revenue: {total_revenue}EUR",
-        (10, 30 + 100),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 0, 0),
-        2,
-    )
-    cust_amount_text_1 = f"""Customers: \nCheckout: {checkout.customers_present} \nDairy: {dairy.customers_present} \nDrinks: {drinks.customers_present} \n"""
-    y0, dy = 30, 40
-    for i, line in enumerate(cust_amount_text_1.split('\n')):
-        y = y0 + i * dy
-        cv2.putText(frame, line, (550, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    x0 = 500
+    y0, dy = 0, 40
+    for i, line in enumerate(customers_screen_text.split("\n")):
+        if i >= 5:
+            x0 = 750
+            y0 = 80
+            y_updated = y0 + (i - 5) * dy
+        else:
+            y_updated = y0 + i * dy
+        write_text(line, x0, y_updated)
 
-    cust_amount_text_2 = f"""Fruit: {fruit.customers_present} \nSpices: {spices.customers_present}"""
-    y0, dy = 70, 40
-    for i, line in enumerate(cust_amount_text_2.split('\n')):
-        y = y0 + i * dy
-        cv2.putText(frame, line, (800, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-
-    frame[47 + 136: 87 + 136, 500: 540] = doodl
+    frame[47 + 136: 87 + 136, 500:540] = doodl
 
     cv2.imshow("frame", frame)
     print(current_time)
     customers.clear()
     current_time += pd.to_timedelta(1, unit="min")
 
-    if cv2.waitKey(1) & 0xFF == ord("n"):
-        customers.append(Customer(img, 800, 700))
-    if cv2.waitKey(1) & 0xFF == ord("m"):
-        customers.append(Customer(img, 800, 700))
-    if cv2.waitKey(1) & 0xFF == ord("k"):
-        customers.append(Customer(img, 800, 700))
-    if cv2.waitKey(1) & 0xFF == ord("l"):
+    if cv2.waitKey(1) & 0xFF == ord("d"):
         cv2.destroyAllWindows()
